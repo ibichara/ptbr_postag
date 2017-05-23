33453cadd36cdbfb6b1bceb66cfa8d126e3fd7c9
Igor Bichara <igor.coutinho@tvglobo.com.br>
2017-05-23 15:17:41 -0300

    new files for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name ="ptbr_postag",
+	packages = ["ptbr_postag"],
+	version = "0.1",
+	description = "Part-of-Speech tagging module for brazilian portuguese language",
+	author = "Igor Bichara",
+	author_email = "igor.coutinho@tvglobo.com.br",
+	url = "https://github.com/ibichara/ptbr_postag",
+	download_url = "https://github.com/ibichara/ptbr_postag/archive/0.1.tar.gz",
+	keywords = ['nlp', 'postag', 'postagging'],
+	classifiers = [])
