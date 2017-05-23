2eea82ec249cefbe5f33a4e172a2208c3c5cba5b
Igor Bichara <igor.coutinho@tvglobo.com.br>
2017-05-23 15:50:01 -0300

    fixing version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
 	name ="ptbr_postag",
 	packages = ["ptbr_postag"],
-	version = "0.1",
+	version = "0.2",
 	description = "Part-of-Speech tagging module for brazilian portuguese language",
 	author = "Igor Bichara",
 	author_email = "igor.coutinho@tvglobo.com.br",
 	url = "https://github.com/ibichara/ptbr_postag",
-	download_url = "https://github.com/ibichara/ptbr_postag/archive/0.1.tar.gz",
+	download_url = "https://github.com/ibichara/ptbr_postag/archive/0.2.tar.gz",
 	keywords = ['nlp', 'postag', 'postagging'],
 	classifiers = [])
